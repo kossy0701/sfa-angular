818ad1bf4e45011d50e7e3f44f3ec01edb25f8ef
kossy0701 <0421kossy0421@gmail.com>
2020-06-25 22:55:27 +0900

    add getIps

--- src/app/components/ips/ips.component.ts
+++ src/app/components/ips/ips.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ip } from '../../models/ip';
+import { IpService } from 'src/app/services/ip.service';
 
 @Component({
   selector: 'app-ips',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ips.component.scss']
 })
 export class IpsComponent implements OnInit {
+  ips: Ip[];
+  displayedColumns = ['IPアドレス', '設定日'];
 
-  constructor() { }
+  constructor(private ipService: IpService) { }
 
   ngOnInit(): void {
+    this.ipService.getIps().subscribe(data => {
+      this.ips = data;
+    });
   }
 
 }
--- src/app/models/ip.ts
+++ src/app/models/ip.ts
@@ -1,9 +1,11 @@
 export class Ip {
   id: number;
   content: string;
+  settedAt: string;
 
-  constructor(id: number, content: string) {
+  constructor(id: number, content: string, settedAt: string) {
     this.id = id;
     this.content = content;
+    this.settedAt = settedAt;
   }
 }
