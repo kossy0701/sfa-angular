233003cd5db07b7fd2786d88c399dc07f343f8c6
kossy0701 <0421kossy0421@gmail.com>
2020-07-09 19:40:50 +0900

    add function of import user from csv

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { SnackBarService } from '../../utils/snack-bar.service';
 import { User } from 'src/app/models/user';
 
 @Component({
@@ -9,8 +10,10 @@ import { User } from 'src/app/models/user';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  loading = false;
+  @ViewChild('fileInput') fileInput;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private snackBarService: SnackBarService) { }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(data => {
@@ -18,4 +21,22 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  importUsersFromCsv(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  import(): void {
+    this.loading = true;
+    const fileBrowser = this.fileInput.nativeElement;
+    if (fileBrowser.files && fileBrowser.files[0]) {
+      this.userService.importUsersFromCsv(fileBrowser.files[0]).subscribe(data => {
+        this.users = data;
+        this.snackBarService.openSnackBar('アップロードに成功しました。', 'OK');
+        this.loading = false;
+      }, err => {
+        this.snackBarService.openSnackBar('アップロードに失敗しました。', 'OK');
+        this.loading = false;
+      });
+    }
+  }
 }
