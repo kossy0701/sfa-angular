ae6ce727304a0530e8c1770e03dbc035251f34e7
kossy0701 <0421kossy0421@gmail.com>
2020-08-15 11:42:16 +0900

    define request + response interface

--- src/app/models/contact.ts
+++ src/app/models/contact.ts
@@ -1,7 +1,50 @@
 export class Contact {
   id: number;
+  contactedAt: string;
+  way: string;
+  purpose: string;
+  subject: string;
+  content: string;
+  target: string;
+  createdAt: string;
 
-  constructor(id: number) {
+  constructor(
+    id: number,
+    contactedAt: string,
+    way: string,
+    purpose: string,
+    subject: string,
+    content: string,
+    target: string,
+    createdAt: string
+  ) {
     this.id = id;
+    this.contactedAt = contactedAt;
+    this.way = way;
+    this.purpose = purpose;
+    this.subject = subject;
+    this.content = content;
+    this.target = target;
+    this.createdAt = createdAt;
   }
+}
+
+export interface ContactForRequest {
+  contactedAt: string;
+  way: string;
+  purpose: string;
+  subject: string;
+  content: string;
+  target: string;
+}
+
+export interface ContactForResponse {
+  id: number;
+  contacted_at: string;
+  way: string;
+  purpose: string;
+  subject: string;
+  content: string;
+  target: string;
+  created_at: string;
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Contact } from '../models/contact';
+import { Contact, ContactForResponse } from '../models/contact';
 import { environment } from '../../environments/environment';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
@@ -20,6 +20,13 @@ export class ContactService {
             const keys = res[key];
             return new Contact(
               keys.id,
+              keys.contacted_at,
+              keys.way,
+              keys.purpose,
+              keys.subject,
+              keys.content,
+              keys.target,
+              keys.createdAt
             );
           });
         } else {
@@ -28,6 +35,23 @@ export class ContactService {
       }),
       catchError((err) => {
         return throwError(err);
+      })
+    );
+  }
+
+  createContact(customerId: number, params): Observable<Contact> {
+    return this.httpClient.post(`${environment.apiBase}/customers/${customerId}/contacts`, params).pipe(
+      map((res: ContactForResponse) => {
+        return new Contact(
+          res.id,
+          res.contacted_at,
+          res.way,
+          res.purpose,
+          res.subject,
+          res.content,
+          res.target,
+          res.created_at
+        );
       })
     );
   }
